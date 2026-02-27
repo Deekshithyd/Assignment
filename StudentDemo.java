521d594d154de887659d47a73ea8a1b6a95dea18
Deekshith YD <deekshithyd75@gmail.com>
2026-02-27 20:51:27 +0530

    Remove unnecessary comments and clean up code

--- StudentDemo.java
+++ StudentDemo.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author deekshith
- */
 class student
 {
     int rollno, sem;
@@ -34,3 +25,4 @@ public class StudentDemo {
         s2.putdata();
     }
 }
+
